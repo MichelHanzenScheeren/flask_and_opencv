72bb9684fd3605dd1c6b1c652d7e5bbc9ccc08f0
MichelScheerenUTFPR <michelscheeren@alunos.utfpr.edu.br>
2020-02-26 23:48:43 -0300

    webcam and rectangle classes modifications

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,12 +1,7 @@
 from flask import Response, render_template
 from app.models.rectangle import Rectangle
 from app.models.webcam import Webcam
-"""
-from imutils.video import VideoStream
-from imutils import resize
-from threading import Thread, Lock
-import cv2.cv2 as cv2
-"""
+
 
 def configure(app):
     @app.route('/')
@@ -14,11 +9,9 @@ def configure(app):
     def index():
         return render_template("index.html")
 
-
     @app.route("/play_webcam")
     def play_webcam():
         return Response(webcam.generate_jpg(rectangle), mimetype = "multipart/x-mixed-replace; boundary=frame")
-
 
     @app.route('/drawing', methods = ['POST'])
     @app.route('/drawing/<int:x1>/<int:y1>/<int:x2>/<int:y2>', methods = ['POST'])
@@ -34,52 +27,4 @@ def stop_stream():
 rectangle = Rectangle()
 webcam = Webcam()
 webcam.start()
-"""
-def get_image():
-    global vs, outputFrame, lock_frame
-    while True:
-        frame = vs.read()  # obtêm frame da Webcam
-        #frame = vs.read()[1]
-        frame = cv2.flip(frame, 1)  # espelha a imagem
-        frame = resize(frame, width=520)  # redimensiona
-        #frame = retangulo(frame)
-        with lock_frame:
-            outputFrame = frame.copy()
 
-
-def generate():
-    global outputFrame, lock_frame
-    while True:
-        with lock_frame:
-            if outputFrame is None:
-                continue
-            (flag, encodedImage) = cv2.imencode(".jpg", draw_rectangle(outputFrame.copy())) # encode the frame in JPEG format
-            if not flag: # ensure the frame was successfully encoded
-                continue
-        # yield the output frame in the byte format
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
-
-
-def draw_rectangle(frame):
-    global x_inicio, x_fim, y_inicio, y_fim
-    with lock_drawing:
-        if x_inicio != -1 and x_fim != -1:
-            cv2.rectangle(frame, (x_inicio, y_inicio), (x_fim, y_fim), (0, 0, 255), 1)
-    return frame
-
-
-def stop_stream():
-    global vs
-    vs.stop()
-
-x_inicio, y_inicio, x_fim, y_fim = -1, -1, -1, -1
-
-outputFrame = None
-lock_frame = Lock()
-lock_drawing = Lock()
-
-vs = VideoStream(src=0).start() #vs = cv2.VideoCapture(0)
-
-t = Thread(target=get_image, daemon=True)
-t.start()
-"""
