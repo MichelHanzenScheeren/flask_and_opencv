a5970228f691742df4b09a053ac869e1957a95bb
MichelScheerenUTFPR <michelscheeren@alunos.utfpr.edu.br>
2020-08-13 11:30:17 -0300

    Melhorias nos métodos que exibem o toast. adição de método que o remove da tela.

--- app/templates/src/analyze.js
+++ app/templates/src/analyze.js
@@ -1,35 +1,51 @@
 document.getElementById("buttonGetDifferentiator").setAttribute("onclick", "get_differentiator()");
 
 window.addEventListener("pageshow", function(_) {
-  if(performance.navigation.type == 2) {
-    location.reload(true);
-  }
+    if(performance.navigation.type == 2) {
+        location.reload(true);
+    }
 });
 
 function config_analyze() {
-  document.getElementById("buttonGetDifferentiator").disabled = true;
-  document.getElementById("refresh_button").disabled = true;
-  document.getElementById("clear_button").disabled = true;
-  document.getElementById("div1").style.display = "none";
-  document.getElementById("div2").style.display = "block";
+    document.getElementById("buttonGetDifferentiator").disabled = true;
+    document.getElementById("refresh_button").disabled = true;
+    document.getElementById("clear_button").disabled = true;
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "block";
 }
 
 async function get_differentiator(){
-  let response = await axios.post("{{ url_for('get_differentiator') }}");
-  if(response.data[0] == "[") return;
-  document.getElementById("red").innerHTML = "Red: " + response.data[0];
-  document.getElementById("green").innerHTML = "Green: " + response.data[1];
-  document.getElementById("blue").innerHTML = "Blue: " + response.data[2];
-  document.getElementById("start").disabled = false;
+    let response = await axios.post("{{ url_for('get_differentiator') }}");
+    if(response.data[0] == "[") return;
+    document.getElementById("red").innerHTML = "Red: " + response.data[0];
+    document.getElementById("green").innerHTML = "Green: " + response.data[1];
+    document.getElementById("blue").innerHTML = "Blue: " + response.data[2];
+    document.getElementById("start").disabled = false;
 
-  get_differentiator_image()
+    get_differentiator_image()
 }
 
 async function get_differentiator_image() {
-  let response = await axios.post("{{ url_for('get_differentiator_image') }}");
-  if(response.data != "") {
-    document.getElementById("differentiator_image").setAttribute("src", "data:image/jpeg;base64," + response.data);
-  }
+    let response = await axios.post("{{ url_for('get_differentiator_image') }}");
+    if(response.data == "") return; 
+
+    let differentiator_image = document.getElementById("differentiator_image")
+    differentiator_image.setAttribute("src", "data:image/jpeg;base64," + response.data);
 }
 
-$('.toast').toast('show')
+(() => {
+    let got_image = ("{{ frame_controll['got_image'] }}");
+    if(got_image == "False") {
+        $(".toast").toast("show");
+    } else {
+        document.getElementById("my-toast").innerHTML = "" //remove da tela, para que não atrapalhe
+    }
+}) () // Função auto executada
+
+$('.toast').on('hidden.bs.toast', function () {
+    let my_toast = document.getElementById("my-toast");
+    if(my_toast == undefined) return;
+    my_toast.innerHTML = ""  //remove da tela, para que não atrapalhe
+}) 
+
+
