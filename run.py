45522912fa7350a8373d9bbd1c5347ad139a353c
MichelHanzenScheeren <michelhanzenscheeren@gmail.com>
2020-10-20 22:57:29 -0300

    Melhorias no arquivo run, que agora aceita argumentos na execução via prompt.

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+import argparse
 from app import create_app
 
 
 if __name__ == '__main__':
-    create_app().run(debug=True, threaded=True, use_reloader=True)
+    my_args = argparse.ArgumentParser()
+    my_args.add_argument('--host', type=str, default='127.0.0.1', help='ip address. 0.0.0.0 to network local acces')
+    my_args.add_argument('--port', type=int, default=5000, help='port number server')
+    args = vars(my_args.parse_args())
+
+    create_app().run(
+        host=args['host'], 
+        port=args['port'], 
+        debug=True, 
+        threaded=True, 
+        use_reloader=True
+    )
