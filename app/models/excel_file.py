cdaba014f6b4acd5cb6e8e44570db1929c1bf5ee
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2020-11-18 16:41:28 -0300

    Criação das docstrings  das classes Frame e ExcelFile.

--- app/models/excel_file.py
+++ app/models/excel_file.py
@@ -5,14 +5,26 @@ from io import BytesIO
 from base64 import b64encode
 
 class ExcelFile():
+  """ Classe que encapsula toda a lógica de criação e personalização do arquivo xlsx.
+
+  Tambem encapsula os usos da biblioteca openpyxl.
+  O arquivo gerado é convertido em bytes na BASE_64.
+  """
+
+
   def __init__(self, title = ''):
-    self.excel_file = Workbook()
-    self.spreadsheet = self.excel_file.active
-    self.spreadsheet.title = title
-    self.to_merge_cells = []
+    self.excel_file = Workbook() # Arquivo excel criado com auxílio da bilioteca openpyxl.
+    self.spreadsheet = self.excel_file.active # Criação de um spreadsheet (página de um excel).
+    self.spreadsheet.title = title # definição do títlo do spreadsheet (recebido por parâmetro na instanciação da classe).
+    self.to_merge_cells = [] # Lista de células que irão compor o spreadsheet. 
   
 
   def create(self, general_info, differentiator_info, captures_info):
+    """ Método responsável por conduzir a personalização do arquivo de acordo com as informações recebidas. 
+    
+    Os parâmetros são listas de listas contendo títulos e dados.
+    Retorna um arquivo xlsx codificado em bytes na base_64.
+    """
     row_count = self.generate_table(general_info, 1)
     row_count = self.generate_table(differentiator_info, row_count)
     row_count = self.generate_table(captures_info, row_count)
@@ -23,6 +35,11 @@ class ExcelFile():
   
 
   def generate_table(self, current_info, row_count):
+    """ Método responsável por receber e escrever as informações no spreeedshet.
+    
+    Recebe como parâmetro as informações que devem ser escritas e o nº da primeira linha que deve ser usada.
+    Retorna um inteiro com a próxima linha válida para continuar registrando informações.
+    """
     font1 = Font(name='Arial', size=14, bold=True, )
     font2 = Font(name='Arial', size=12)
     for i, row in enumerate(current_info):
@@ -37,17 +54,24 @@ class ExcelFile():
   
 
   def configure_columns_width(self):
+    """ Configura o tamanho mínimo das colunas para que infomações não sejam ocultadas. 
+    
+    Os testes feitos apontaram 25 como um bom tamanho.
+    Nenhum valor é retornado.
+    """
     for column in self.spreadsheet.columns:
       self.spreadsheet.column_dimensions[column[0].column_letter].width = 25.0
   
 
   def merge_cells(self):
+    """ Mescla as colunas de título do spreedshet. """
     for value in self.to_merge_cells:
       self.spreadsheet.merge_cells(start_row=value[0], start_column=1, end_row=value[0], end_column=value[1])
       self.spreadsheet.cell(row=value[0], column=1).fill = PatternFill(fgColor='D3D3D3', fill_type = 'solid')
 
 
   def generate_captures_graph(self, captures_info, row_count):
+    """ Gera o gráfico Sinais X Tempo. """
     my_chart = ScatterChart()
     my_chart.title = 'Gráfico dos Sinais'
     my_chart.style = 16
@@ -63,6 +87,7 @@ class ExcelFile():
 
 
   def encode_excel(self, excel_file):
+    """ Codifica o arquivo xlsx em bytes na base_64. """
     encoded = BytesIO()
     excel_file.save(encoded)
     encoded.seek(0)
--- app/models/frame.py
+++ app/models/frame.py
@@ -3,9 +3,15 @@ from app.models.image_pack import ImagePack
 
 
 class Frame:
+  """ Classe que encapsula o armazenamento, get/setters e validações de um frame. 
+  
+  Também possui um lock para evitar possíveis condições de corrida.
+  """
+
+
   def __init__(self):
-    self._frame = None
-    self._lock = Lock()
+    self._frame = None # armazena o frame.
+    self._lock = Lock() # Lock do frame para garantir que condições de corrida não ocorram. 
   
 
   def set_frame(self, new_frame):
@@ -14,6 +20,12 @@ class Frame:
   
 
   def get_copy(self):
+    """ Cria uma cópia do frame atual para ser retornada a aplicação. 
+    
+    Uma cópia é criada (no lugar do frame original) para garantir que o retângulo qeu será desenhado não interfira na análise.
+    Se nenhum frame estiver salvo, retorna None.
+    Caso um frame exista, retorna-o no padrão OpenCV (ndarray).
+    """
     with self._lock:
       if self._frame is None:
         return None
