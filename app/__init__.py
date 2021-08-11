40f7529e8f5edf41850deccbdd93acc693f04d47
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2021-08-11 18:50:05 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.controllers import controller
 
 def create_app():
     """ Criação do projeto e configuração das rotas da aplicação. """
-    
+
     app = Flask(__name__)
     controller.configure_routes(app)
     return app
