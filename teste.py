9e3aeb2fb072496aae425b6d27f4d88611fa7265
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2021-08-27 00:48:07 -0300

    Remoção do arquivo residual de teste.

--- teste.py
+++
@@ -1,8 +0,0 @@
-import numpy
-
-image = numpy.array([[[255, 254, 253], [252, 251, 250]], [[249, 248, 247], [246, 245, 244]]])
-print(image, end='\n****\n')
-
-div = 4
-quantized = image // div * div + div // 2
-print(quantized)
